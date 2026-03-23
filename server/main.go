c996bb6f5df3ee2111361030a981abf11e33d7f7
Charles Carlson <carlson.charles20@gmail.com>
2026-03-23 13:39:32 -0400

    adding boilerplate

--- server/main.go
+++ server/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main(){}
+import (
+	"context"
+	"fmt"
+	"grpc-server/proto"
+	"sync"
+)
+
+// This will log to our metrics in Cloudwatch
+func logger(format string, a ...any) {
+	fmt.Printf("LOG:\t"+format+"\n", a...)
+}
+
+type server struct {
+	proto.UnimplementedServerServer
+	currentDirectory string
+	mu               sync.Mutex
+}
+
+func (s *server) ChangeDirectory(ctx context.Context, in *proto.ChangeDirectoryRequest) (*proto.ChangeDirectoryResponse, error) {
+	//we implement logic here
+	return nil, nil
+}
+
+func main() {
+
+}
