f8154121e23e41f7824433178bd989a1a8706b10
gyuszix <gyuszix@gmail.com>
2026-03-23 13:00:51 -0400

    ecr terraform

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,3 +2,7 @@ output "repository_url" {
   description = "Full URL of the ECR repository"
   value       = aws_ecr_repository.this.repository_url
 }
+
+output "repository_arn" {
+  value = aws_ecr_repository.app.arn
+}
--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -2,3 +2,8 @@ variable "repository_name" {
   description = "The name of the ECR repository"
   type        = string
 }
+
+variable "environment" {
+  type = string
+  description = "Deployment env"
+}
