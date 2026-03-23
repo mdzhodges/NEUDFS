63d5d523c57717900cce984c66f2725373ac3cbb
Jordan Glass <jordanglass@Jordans-MacBook-Pro.local>
2026-03-23 14:05:36 -0400

    added s3 terraform and git ignore

---
+++ terraform/modules/s3_storage/main.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "neudfs_bucket" {
+  bucket = "neudfs-storage-${var.environment}"
+  
+  tags = {
+    Name        = "NEUDFS Storage"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "neudfs_block" {
+  bucket = aws_s3_bucket.neudfs_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# provides durability be versioning our bucket
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.neudfs_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
